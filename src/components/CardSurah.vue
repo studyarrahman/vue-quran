e9a27288bfc40e9e34466e5cf94920160ad59a69
NOVRI RAHMAN <129195286+studyarrahman@users.noreply.github.com>
2023-05-16 22:00:09 +0700

    Update CardSurah.vue

--- src/components/CardSurah.vue
+++ src/components/CardSurah.vue
@@ -48,7 +48,7 @@ computed: {
             <img src="../assets/kaligrafi_quran.png" alt="Kaligrafi" width="130" class="mb-3 mt-5">
             <h2 class="text-center">Search Quran</h2>
             <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Masukkan Nama Surah... (Contoh: al-fatihah)" aria-label="Recipient's username" aria-describedby="button-addon2" v-model="searchQuery" @input="getListSurah">
+            <input type="text" class="form-control" placeholder="Masukkan Nama Surah..." aria-label="Recipient's username" aria-describedby="button-addon2" v-model="searchQuery" @input="getListSurah">
             <button class="btn btn-dark" type="button" id="search-button">Search</button>
             </div>
         </div>
